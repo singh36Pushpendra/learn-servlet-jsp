d2d867805b889ef4b18cb185aadabdb9b5bdb77d
singh36Pushpendra <psthakur.1999@gmail.com>
2022-11-24 13:04:47 +0530

    First Commit

---
+++ src/main/java/com/firstservlet/FirstServlet.java
@@ -0,0 +1,21 @@
+package com.firstservlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/FirstServlet")
+public class FirstServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+
+        try (PrintWriter out = resp.getWriter()) {
+            out.println("<h1>Welcome Pushpendra Singh to FirstServlet!</h1>");
+        }
+    }
+}
