5ab59d67aeb7e7081a76e85fabab072f811e3c17
singh36Pushpendra <psthakur.1999@gmail.com>
2022-11-24 21:08:21 +0530

    Added: UC3 Check name validity

--- src/main/java/com/login/LoginServlet.java
+++ src/main/java/com/login/LoginServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.regex.Pattern;
 
 @WebServlet(
         description = "Login Servlet Demo",
@@ -20,6 +21,11 @@ import java.io.PrintWriter;
         }
 )
 public class LoginServlet extends HttpServlet {
+
+    static boolean validate(final String regex, String input) {
+        return Pattern.compile(regex).matcher(input).matches();
+    }
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // Getting parameter values from login.html page.
@@ -33,17 +39,20 @@ public class LoginServlet extends HttpServlet {
         String uname = servletConfig.getInitParameter("username");
         String upwd = servletConfig.getInitParameter("password");
 
-        // Checking user credentials.
-        if (uname.equals(user) && upwd.equals(pwd)) {
-            req.setAttribute("user", user);
-            req.getRequestDispatcher("LoginSuccess.jsp").forward(req, resp);
-        }
-        else {
-            RequestDispatcher rd = getServletContext().getRequestDispatcher("login.html");
-            try (PrintWriter out = resp.getWriter()) {
-                out.println("<font color=red>Incorrect username or password login!</font>");
-                rd.include(req, resp);
+        if (validate("^[A-Z][a-z]{2}*$", user))
+            // Checking user credentials.
+            if (uname.equals(user) && upwd.equals(pwd)) {
+                req.setAttribute("user", user);
+                req.getRequestDispatcher("LoginSuccess.jsp").forward(req, resp);
             }
-        }
+            else {
+                RequestDispatcher rd = getServletContext().getRequestDispatcher("login.html");
+                try (PrintWriter out = resp.getWriter()) {
+                    out.println("<font color=red>Incorrect username or password login!</font>");
+                    rd.include(req, resp);
+                }
+            }
+            else
+                req.getRequestDispatcher("login.html").forward(req, resp);
     }
 }
